18ece8627d61871120bc67c251e1d472b14146cf
zakaer <jasonvo1@live.com>
2016-07-10 17:26:18 -0500

    Add files via upload

---
+++ numberguessinggame.py
@@ -0,0 +1,31 @@
+#Author -- Zakaer
+
+import random
+
+print("Welcome to the Guessing game!")
+print("The computer has generated a number between 1&100 that you must guess.")
+
+def main():
+    tries = 1
+    randomnumber = random.randrange(0,101) #Generating random number
+    print("Enter a guess:")
+    while True:
+        guess = int(input("")) #Takes user's guess
+        
+        if guess == randomnumber: #user gets correct
+            print("You have correctly guessed the number. It was {} and it took you {} tries.".format(randomnumber, tries))
+            
+            yn = input("Would you like to play again? (yes/no)").lower() #Asks user if they want to reply.
+            if yn == "yes":
+                main()
+            else:
+                print("Goodbye!")
+                break
+                
+        elif guess > randomnumber: #user's guess is bigger than the generated number
+            print("Lower\n")
+            tries = tries+1
+        elif guess < randomnumber: #users guess is smaller than the generated number
+            print("Higher\n")
+            tries = tries+1
+main()
