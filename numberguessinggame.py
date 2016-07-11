693cae768b8c0e4d11bbd43ca8da0298d180f1fe
zakaer <jasonvo1@live.com>
2016-07-10 21:42:28 -0500

    Fixing ValueError for input

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -10,7 +10,12 @@ def main():
     randomnumber = random.randrange(0,101) #Generating random number
     print("Enter a guess:")
     while True:
-        guess = int(input("")) #Takes user's guess
+        try:
+            guess = int(input("")) #Takes user's guess
+        except ValueError:
+            print("Try entering a valid number.")
+            continue
+
         
         if guess == randomnumber: #user gets correct
             print("You have correctly guessed the number. It was {} and it took you {} tries.".format(randomnumber, tries))
